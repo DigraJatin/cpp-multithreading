04d3e45384b5729cef2f3bf3b58cf83404cfabc5
Jatin <digrajatin@gmail.com>
2025-09-07 01:19:33 +0530

    Added output examples and explanation for easy 1,2

--- easy/01_print_hello_from_threads.cpp
+++ easy/01_print_hello_from_threads.cpp
@@ -20,3 +20,20 @@ int main() {
     t2.join();
     return 0;
 }
+
+/* Now, the output of this program when run will not be what you might be expecting.
+
+If you are thinking it will be - 
+Hello from thread 1
+Hello from thread 2
+
+Then you are wrong. The output can very random because the threads are running concurrently, it can be like -
+Hello from thread Hello from thread 21
+OR
+Hello from thread Hello from thread 2
+1
+OR something else, we don't know what exactly.
+
+This is because the two threads may interleave their execution in unpredictable ways.
+We will learn how to control this behavior in the next examples.
+*/
--- easy/02_thread_join_vs_detach.cpp
+++ easy/02_thread_join_vs_detach.cpp
@@ -10,13 +10,45 @@ If the main thread exits before the detached thread finishes, the detached threa
 #include <thread>
 
 void task() {
-    std::cout << "Running task in background\n";
+    std::cout << "Running task in background \n";
+    for(int i = 0; i < 5; ++i) {
+        std::cout << "Task running: " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 }
 
 int main() {
     std::thread t(task);
-    //t.join(); // Join the thread to wait for it to finish
+    // t.join(); // Join the thread to wait for it to finish
     
     t.detach(); // Detach the thread to run independently till the main thread exits
+    // sleeping for 1 second to allow some output from the detached thread
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Main thread finishing \n";
     return 0;
 }
+
+/* 
+The output of above program when run - 
+1. When line t.join(); is commented out, the main thread finishes without waiting for 
+the t thread to finish it's work, we might see output like below or something else depending
+on how much work the thread was able to complete before main thread finished.
+
+Running task in background 
+Task running: 0
+Task running: 1
+Main thread finishing 
+
+2. When line t.join(); is uncommented & t.detach(); is commented, the main thread waits for the t thread to finish its work before exiting, we will see complete output from the t thread.
+Running task in background we see complete output from the t thread.
+
+Running task in background 
+Task running: 0
+Task running: 1
+Task running: 2
+Task running: 3
+Task running: 4
+Main thread finishing 
+
+This shows the difference between join and detach in C++ threads.
+*/
